a7dfb304d3aecc4592a3ec5100f7375b116bc598
rodrigoperezpineda <roperez.pineda@gmail.com>
2025-09-13 21:43:14 -0300

    Responsividade e alterações no footer e navbar

--- CÓDIGOS/ContactPage/contactPage.js
+++ CÓDIGOS/ContactPage/contactPage.js
@@ -24,3 +24,19 @@ function enviarForm() {
       });
     });
 }
+
+const btnHamburguer = document.getElementById("hamburguer");
+const icon = document.getElementById("hamburguer-icon");
+const navList = document.getElementById("nav-list");
+
+btnHamburguer.addEventListener("click", () => {
+  navList.classList.toggle("show");
+
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  } else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+});
--- Testes/gemini.js
+++ Testes/gemini.js
@@ -1,16 +1,15 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  
-  
-  
+  apiKey: "AIzaSyBf86TcfWqO4YTkAZPV58YMjqE0ZL-rbig"
 });
 
 async function main() {
+  var frase = 'I see you once in a blue moon'
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents:
-      "Translate this sentence into portuguese, while also making sure that the meaning of the sentence is maintained, make possible adaptations when necessaries for expressions and slang in general: 'My bro got cooked'",
+      "Translate this sentence into portuguese, while also making sure that the meaning of the sentence is maintained, make possible adaptations when necessaries for expressions and idioms in general:" + frase,
   });
   let resposta = response.text;
   console.log(resposta);
